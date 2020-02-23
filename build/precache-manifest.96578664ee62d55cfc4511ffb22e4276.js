52661c54a0daeba265c39a6f8815490df32f4bd1
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2020-02-23 23:09:25 +0500

    remove logs

---
+++ build/precache-manifest.96578664ee62d55cfc4511ffb22e4276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814c1766241231c933450703928c824a",
+    "url": "/Tablegenerator/index.html"
+  },
+  {
+    "revision": "bdb776319e3e8c394f47",
+    "url": "/Tablegenerator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bb64122a13b0788d74b",
+    "url": "/Tablegenerator/static/js/2.81da5c01.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tablegenerator/static/js/2.81da5c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb776319e3e8c394f47",
+    "url": "/Tablegenerator/static/js/main.7a190d6e.chunk.js"
+  },
+  {
+    "revision": "ffa9303008f8ada351ab",
+    "url": "/Tablegenerator/static/js/runtime-main.2de66e4b.js"
+  }
+]);
